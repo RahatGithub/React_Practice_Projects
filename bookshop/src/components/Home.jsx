2c216e9fba7eaf3cddef24f934cbee85a65d9d0a
Rahat Ahmed Chowdhury <rahatahmedchowdhuryrac@gmail.com>
2024-05-18 13:57:02 +0600

    bookshop> Login page done

--- bookshop/src/components/Home.jsx
+++ bookshop/src/components/Home.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import '../css/Home.css'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="hero">
+      <div className="hero-content">
+        <h1 className="hero-text">Book Shop</h1>
+        <h3>Browse the collection of our top best interesting books. You will definitely find what you are looking for. </h3>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis, maxime omnis. Minima harum a ducimus voluptates alias similique omnis tempora qui voluptas hic natus, quam magnam amet mollitia suscipit repellat corrupti exercitationem fugiat iste dicta maiores nisi explicabo. A libero cumque modi dolorum quos excepturi nemo eaque, culpa nulla placeat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias tempora ex numquam assumenda officiis quas dolorem explicabo, aut dicta laborum! Molestias deleniti aspernatur nam excepturi, laborum aliquid dolorum voluptate praesentium.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, enim incidunt tenetur eligendi cupiditate nihil saepe laborum quidem sed eveniet facere asperiores cum illo perferendis magni aliquam earum, nobis, repudiandae perspiciatis corporis ut. Ipsa alias molestiae eaque dicta ea voluptatem officia, temporibus accusantium a! Illo, asperiores atque! Mollitia, ad veniam!</p>
+      </div>
+      <div className="hero-image">
+        <img src="Images/HomePage.jpg" className='hero-image' alt="HomePage" />
+      </div>
+    </div>
   )
 }
