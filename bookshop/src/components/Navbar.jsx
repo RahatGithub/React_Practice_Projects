54c578f0aafbcb16a299ab439a6e711cf6803aed
Rahat Ahmed Chowdhury <rahatahmedchowdhuryrac@gmail.com>
2024-05-18 12:54:39 +0600

    bookshop> minimum stlyles added

--- bookshop/src/components/Navbar.jsx
+++ bookshop/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../css/Navbar.css'
 
 export default function Navbar() {
   return (
     <nav className='navbar'>
       <div className='navbar-left'>
-        <span>Book Store</span>
+        <span className='navbar-brand'>Book Store</span>
       </div>
       <div className='navbar-right'>
         <Link to="/" className='navbar-links'>Home</Link>
